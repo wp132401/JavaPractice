47ebc1000e1d4d72e43f999304378d11c3dd4326
Ko ki hyeon <Ko ki hyeon@172.17.2.196>
2015-03-16 09:35:45 +0900

    수정본

--- JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -18,7 +18,7 @@ public class PrimitiveDataType {
 		
 		System.out.println("<< 아이유 프로필 >>\n");
 		System.out.println("이름 : "+name);
-		System.out.printf("성별 : %s\n", gender1);
+		System.out.println("성별 : "+ gender1);
 		System.out.printf("나이 : %d세\n", age);
 		System.out.printf("키 : %.1fcm\n", hight);
 		System.out.printf("몸무게 : %.1fkg\n", weight);
